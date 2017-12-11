624708eaf91bd60030823068da203a9bf55154a1
Andrey Tyshev <slowprog.job@gmail.com>
2017-12-11 07:22:14 +0300

    Add lesson 3

--- algorithms/main.c
+++ algorithms/main.c
@@ -1,152 +1,251 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string.h>
 
-int calculateRec(int from, int to);
-int calculateArr(int from, int to);
-void calculateTask3(void);
-char* convert(int number);
-void convertTask1(void);
-int powRec(int number, int pow);
-void powerTask2(void);
-int powEven(int number, int pow);
-int powArr(int number, int pow);
-
-int main(int argc, const char * argv[])
+void swap(int *a, int *b)
 {
-    convertTask1();
-    powerTask2();
-    calculateTask3();
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-void convertTask1()
+void printArray(int *a, int size)
 {
-    printf("Task 1: convert\n");
-    
-    printf("%d to bin: %s\n", 1, convert(1));
-    printf("%d to bin: %s\n", 106, convert(106));
-    printf("%d to bin: %s\n", 7492, convert(7492));
-}
-
-char* convert(int number)
-{
-    char string[100] = "";
-    
-    if (number > 1) {
-        strcpy(string, convert(number / 2));
+    for (int i = 0; i < size; i++) {
+        printf("%2i ", a[i]);
     }
-    
-    return strcat(string, number % 2 == 0 ? "0" : "1");
 }
 
-void powerTask2()
+void fillArray(int *a, int size, int isRandom)
 {
-    printf("\nTask 2: power\n");
-    
-    printf("Arr power %d to %d: %d\n", 4, 3, powArr(4, 3));
-    printf("Rec power %d to %d: %d\n", 4, 3, powRec(4, 3));
-    printf("Even power %d to %d: %d\n", 4, 3, powEven(4, 3));
-    printf("---\n");
-    printf("Arr power %d to %d: %d\n", 5, 4, powArr(5, 4));
-    printf("Rec power %d to %d: %d\n", 5, 4, powRec(5, 4));
-    printf("Even power %d to %d: %d\n", 5, 4, powEven(5, 4));
+    for (int i = 0; i < size; i++) {
+        a[i] = isRandom ? ((int)rand() % 100) : i;
+    }
 }
 
-int powArr(int number, int pow)
+/**
+ * Обычная сортировка пузырьком.
+ */
+void bubbleClassic(int *array, int size)
 {
-    if (pow == 0) {
-        return 1;
-    }
+    printf("\nTask 1a: bubble classic\n");
     
-    int result = number;
+    printf("%-16s", "Array: ");
+    printArray(array, size);
+    printf("\n");
     
-    for (int i = 0; i < pow - 1; i++) {
-        result *= number;
-    }
-    
-    return result;
-}
-
-int powRec(int number, int pow)
-{
-    if (pow == 0) {
-        return 1;
-    }
-    
-    if (pow > 1) {
-        return number * powRec(number, pow - 1);
-    }
-    
-    return number;
-}
-
-int powEven(int number, int pow)
-{
-    if (pow == 0) {
-        return 1;
-    }
-    
-    if (pow > 1) {
-        if (pow % 2 == 0) {
-            return powRec(number * number, pow / 2);
-        } else {
-            return number * powRec(number * number, pow / 2);
-        }
-    }
-    
-    return number;
-}
-
-void calculateTask3()
-{
-    printf("\nTask 3: calculate\n");
-    
-    printf("from %d to %d: %d\n", 1, 1, calculateRec(1, 1));
-    printf("from %d to %d: %d\n", 1, 4, calculateRec(1, 4));
-    printf("from %d to %d: %d\n", 3, 20, calculateRec(3, 20));
-    
-//    printf("from %d to %d: %d\n", 1, 1, calculateArray(1, 1));
-//    printf("from %d to %d: %d\n", 1, 4, calculateArray(1, 4));
-//    printf("from %d to %d: %d\n", 3, 20, calculateArray(3, 20));
-}
-
-//       1
-//      / \
-//     2   2
-//    /|   |\
-//   3 4   3 4
-//  / \   / \
-// 4   6 4   6
-int calculateRec(int from, int to)
-{
-    if (from == to) {
-        return 1;
-    } else if (from > to) {
-        return 0;
-    }
-    
-    return calculateRec(from + 1, to) + calculateRec(from * 2, to);
-}
-
-int calculateArray(int from, int to)
-{
+    int i;
+    int j;
     int count = 0;
-
-    for (int i = from; i <= to; i++) {
-        if (i == to) {
+    
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size - 1; j++) {
             count++;
-        } else if (i * 2 == to) {
-            count++;
+            if (array[j] > array[j + 1]) {
+                swap(&array[j], &array[j + 1]);
+            }
         }
     }
     
-    for (int i = from; i <= to; i *= 2) {
-        if (i == to) {
-            count++;
-        } else if (i + 1 == to) {
-            count++;
-        }
-    }
-
-    return count;
+    printf("%-16s", "Sorted: ");
+    printArray(array, size);
+    printf("– hits %d (O(n^2) = %d)\n", count, size * size);
 }
 
+/**
+ * Сортировка пузырьком оканчивающаяся раньше, если была итерация без перемещений.
+ */
+void bubbleFast(int *array, int size)
+{
+    printf("\nTask 1b: bubble fast\n");
+    
+    printf("%-16s", "Array: ");
+    printArray(array, size);
+    printf("\n");
+    
+    int i;
+    int j;
+    int count = 0;
+    int found = 0;
+    
+    for (i = 0; i < size; i++) {
+        found = 0;
+        for (j = 0; j < size - 1; j++) {
+            count++;
+            if (array[j] > array[j + 1]) {
+                swap(&array[j], &array[j + 1]);
+                found = 1;
+            }
+        }
+        if (!found) {
+            break;
+        }
+    }
+    
+    printf("%-16s", "Sorted: ");
+    printArray(array, size);
+    printf("– hits %d (O(n^2) = %d)\n", count, size * size);
+}
+
+/**
+ * Обычная шейкерная сортировка.
+ */
+void shakeClassic(int *array, int size)
+{
+    printf("\nTask 2a: shake classic\n");
+    
+    printf("%-16s", "Array: ");
+    printArray(array, size);
+    printf("\n");
+    
+    int i;
+    int j;
+    int count = 0;
+    
+    for (i = 0; i < size; i++) {
+        for (j = i; j < size - 1 - i; j++) {
+            count++;
+            if (array[j] > array[j + 1]) {
+                swap(&array[j], &array[j + 1]);
+            }
+        }
+        
+        for (j = size - 1 - i; j > i; j--) {
+            count++;
+            if (array[j] < array[j - 1]) {
+                swap(&array[j], &array[j - 1]);
+            }
+        }
+    }
+    
+    printf("%-16s", "Sorted: ");
+    printArray(array, size);
+    printf("– hits %d (O(n^2) = %d)\n", count, size * size);
+}
+
+/**
+ * Шейкерная сортировка оканчивающаяся раньше, если была итерация без перемещений.
+ */
+void shakeFast(int *array, int size)
+{
+    printf("\nTask 2b: shake fast\n");
+    
+    printf("%-16s", "Array: ");
+    printArray(array, size);
+    printf("\n");
+    
+    int i;
+    int j;
+    int count = 0;
+    int found = 0;
+    
+    for (i = 0; i < size; i++) {
+        found = 0;
+        for (j = i; j < size - 1 - i; j++) {
+            count++;
+            if (array[j] > array[j + 1]) {
+                swap(&array[j], &array[j + 1]);
+                found = 1;
+            }
+        }
+        
+        if (!found) {
+            break;
+        }
+        
+        for (j = size - 1 - i; j > i; j--) {
+            count++;
+            if (array[j] < array[j - 1]) {
+                swap(&array[j], &array[j - 1]);
+                found = 1;
+            }
+        }
+        
+        if (!found) {
+            break;
+        }
+    }
+    
+    printf("%-16s", "Sorted: ");
+    printArray(array, size);
+    printf("– hits %d (O(n^2) = %d)\n", count, size * size);
+}
+
+/**
+ * Бинарный поиск в массиве.
+ */
+void binarySearch(int *array, int size, int needle)
+{
+    printf("\nTask 3: binary search (range 0..%d) – %d\n", size - 1, needle);
+    
+    int first = 0;
+    int last = size;
+    int count = 0;
+    int center = last / 2;
+    
+    while (first < last) {
+        count++;
+        // 0 1 2 3 4 5 6 7 8 9 10 11 12
+        if (array[center] >= needle) {
+            last = center;
+        } else {
+            first = center + 1;
+        }
+        
+        center = first + (last - first) / 2;
+    }
+    
+    if (array[center] == needle) {
+        printf("%-16s", "Found: ");
+        printf("hits %d\n", count);
+    } else {
+        printf("%-16s", "Not found: ");
+        printf("hits %d\n", count);
+    }
+}
+
+void sortings()
+{
+    int size = 13;
+    int arrayBuuble[size];
+    int arrayBuubleFast[size];
+    int arrayShake[size];
+    int arrayShakeFast[size];
+    
+    fillArray(arrayBuuble, size, 1);
+    
+    memcpy(arrayBuubleFast, arrayBuuble, sizeof(arrayBuuble));
+    memcpy(arrayShake, arrayBuuble, sizeof(arrayBuuble));
+    memcpy(arrayShakeFast, arrayBuuble, sizeof(arrayBuuble));
+    
+    bubbleClassic(arrayBuuble, size);
+    bubbleFast(arrayBuubleFast, size);
+    shakeClassic(arrayShake, size);
+    shakeFast(arrayShakeFast, size);
+}
+
+void searching()
+{
+    int size = 100;
+    int array[size];
+    
+    fillArray(array, size, 0);
+    
+    // Разные варианты искомого, чтобы поглядеть во сколько хитов закончит.
+    binarySearch(array, size, array[0]);
+    binarySearch(array, size, array[size - 1]);
+    binarySearch(array, size, array[size / 2 - 1]);
+    binarySearch(array, size, array[size / 2]);
+    binarySearch(array, size, array[size / 2 + 1]);
+    binarySearch(array, size, size);
+}
+
+int main()
+{
+    srand((int)time(NULL));
+    
+    sortings();
+    searching();
+}
